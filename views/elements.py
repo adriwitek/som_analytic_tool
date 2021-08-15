84a67ff5548d1c055891955d5d5595c9b47084f2
adriwitek <adrianrubio_24@hotmail.com>
2021-08-15 13:44:31 +0200

    added about and presentation

--- views/elements.py
+++ views/elements.py
@@ -3,6 +3,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash_html_components.A import A
 import  views.elements as elements
 
 
@@ -10,6 +11,9 @@ from  views.session_data import Sesion
 from  config.config import *
 
 import base64
+from views.app import app
+from dash.dependencies import Input, Output, State
+import views.plot_utils as pu
 
 
 
@@ -51,9 +55,52 @@ navigation_bar = dbc.Navbar(
             ),
             href="/",
         ),
-        dbc.NavbarToggler(id="navbar-toggler")    
+
+        dbc.Modal(
+            [
+                dbc.ModalHeader("About"),
+                dbc.ModalBody([
+                    html.Div(style=pu.get_css_style_inline_flex(),
+                            children = [
+                                html.H6( dbc.Badge( 'Developer',  pill=True, color="light", className="mr-1",id ='badge_info_percentage_train_slider')   ),
+                                html.H6( dbc.Badge( 'adriwitek',  pill=True, color="info", className="mr-1",id ='badge_info_percentage_test_slider')   )
+                            ]
+                    ),
+                    dbc.Button('https://github.com/adriwitek', color="link",href = 'https://github.com/adriwitek' ),
+                    html.Br(),
+                    html.Br(),
+                    dbc.Button("Close", id="button_close_about", className="ml-auto"),
+                    ],style = pu.get_css_style_center()
+                ),
+            ],
+            id="modal_about",
+            centered=True,
+            is_open= False,
+        ),
+
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Button("About", id= 'button_about',color="link"),
+                    width="auto",
+                ),
+            ],
+            no_gutters=True,
+            className="ml-auto flex-nowrap mt-3 mt-md-0",
+            align="center",
+        ),
+                
+        dbc.NavbarToggler(id="navbar-toggler")   
     ],
     color="white")
 
 
 
+@app.callback(  Output("modal_about", "is_open"),
+                Input("button_about", "n_clicks"), 
+                Input("button_close_about", "n_clicks"), 
+                State("modal_about", "is_open"),
+                prevent_initial_call=True
+)
+def show_about_modal( n_clicks1, n_clicks2, is_open):
+    return (not is_open)
